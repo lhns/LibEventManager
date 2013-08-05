2d394bdfe4c731a43d1f114f12019f7caabf1bdc
pierre <pierrekisters@gmail.com>
2013-08-06 01:14:29 +0200

    IEventListener is now Deprecated

--- src/com/dafttech/eventmanager/IEventListener.java
+++ src/com/dafttech/eventmanager/IEventListener.java
@@ -3,6 +3,7 @@ package com.dafttech.eventmanager;
 /**
  * Use this Interface to create an onEvent Listener
  */
+@Deprecated
 public interface IEventListener {
     public void onEvent(Event event);
 }
