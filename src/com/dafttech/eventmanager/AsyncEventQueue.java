c994818395a389a541c260f5e15a70e9f61232cf
Pierre <pierrekisters@msn.com>
2013-09-30 19:27:56 +0200

    cleaned up the async functionality and fixed it

--- src/com/dafttech/eventmanager/AsyncEventQueue.java
+++ src/com/dafttech/eventmanager/AsyncEventQueue.java
@@ -4,22 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AsyncEventQueue {
-    volatile protected List<Event> eventQueue;
-    volatile private Thread queueThread;
+    volatile protected List<Event> eventQueue = new ArrayList<Event>();
+    volatile protected QueueThread queueThread;
     volatile private int queueOverflow = 2000;
-
-    protected AsyncEventQueue() {
-        eventQueue = new ArrayList<Event>();
-        queueThread = new QueueThread(this);
-    }
 
     protected void add(Event event) throws QueueOverflowException {
         if (eventQueue.size() >= queueOverflow) throw new QueueOverflowException();
         eventQueue.add(event);
-        notifyThread();
+        sheduleQueue();
     }
 
-    private void notifyThread() {
-        queueThread.start();
+    private void sheduleQueue() {
+        if (queueThread == null || !queueThread.isRunning()) {
+            queueThread = new QueueThread();
+        }
+    }
+
+    private class QueueThread extends Thread {
+        private volatile boolean running;
+
+        public QueueThread() {
+            running = true;
+            start();
+        }
+
+        @Override
+        public void run() {
+            Event firstEvent = null;
+            while (eventQueue.size() > 0) {
+                firstEvent = eventQueue.get(0);
+                if (firstEvent != null) firstEvent.sheduleEvent();
+                if (firstEvent == null || firstEvent.isDone()) eventQueue.remove(0);
+            }
+            running = false;
+        }
+
+        public boolean isRunning() {
+            return running;
+        }
     }
 }
