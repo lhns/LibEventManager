33df4370201d554f9f074bbeb6221777fb867081
LolHens <pierrekisters@gmail.com>
2014-05-28 19:14:13 +0200

    new StackReader

--- src/com/dafttech/reflect/StackReader.java
+++ src/com/dafttech/reflect/StackReader.java
@@ -1,5 +1,46 @@
 package com.dafttech.reflect;
 
-public class StackReader {
+import java.lang.reflect.Method;
 
+public class StackReader {
+    @SuppressWarnings("unused")
+    private Thread thread;
+    private StackTraceElement[] stackTrace;
+
+    public StackReader(Thread thread) {
+        this.thread = thread;
+        stackTrace = thread.getStackTrace();
+    }
+
+    public StackTraceElement[] getStackTrace() {
+        return stackTrace;
+    }
+
+    public Class<?>[] getClasses() {
+        Class<?>[] classes = new Class[stackTrace.length];
+        for (int i = 0; i < stackTrace.length; i++)
+            classes[i] = stackTrace[i].getClass();
+        return classes;
+    }
+
+    public String[] getMethodNames() {
+        String[] methodNames = new String[stackTrace.length];
+        for (int i = 0; i < stackTrace.length; i++)
+            methodNames[i] = stackTrace[i].getMethodName();
+        return methodNames;
+    }
+
+    public Method[] getMethods() {
+        Method[] methods = new Method[stackTrace.length];
+        for (int i = 0; i < stackTrace.length; i++) {
+            try {
+                methods[i] = stackTrace[i].getClass().getDeclaredMethod(stackTrace[i].getMethodName());
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+        return methods;
+    }
 }
