d782ddcc88d01baeaa1464e0c9ab53255cb10677
LolHens <LolHens@users.noreply.github.com>
2014-06-09 22:09:39 +0200

    added TypePrimitive.getSize(); (abstract)

--- src/com/dafttech/type/TypePrimitive.java
+++ src/com/dafttech/type/TypePrimitive.java
@@ -1,6 +1,8 @@
 package com.dafttech.type;
 
 public abstract class TypePrimitive<ClassType> extends Type<ClassType> {
+    public abstract int getSize();
+    
     public abstract long toLong(ClassType val);
 
     public abstract ClassType fromLong(long val);
