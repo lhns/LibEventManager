1066d8c6d6ce15d1abe153fb6e06e63f24e1f2d2
LolHens <pierrekisters@gmail.com>
2014-07-20 20:04:44 +0200

    removed classDiscoverer (still a static method inside ClassLocation)

--- src/com/dafttech/newclassloader/URLClassLocation.java
+++ src/com/dafttech/newclassloader/URLClassLocation.java
@@ -1,7 +1,16 @@
 package com.dafttech.newclassloader;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.dafttech.hash.HashUtil;
@@ -48,9 +57,27 @@ public class URLClassLocation {
         return null;
     }
 
-    // TODO
-    public static Set<URLClassLocation> discoverSourceURL(URL sourceURL) {
-        return null;
+    public static Set<URLClassLocation> discoverSourceURL(final URL sourceURL) {
+        final Set<URLClassLocation> discovered = new HashSet<URLClassLocation>();
+        try {
+            final Path sourcePath = Paths.get(sourceURL.toURI());
+            Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.getFileName().toString().toLowerCase().endsWith(EXT_CLASS)) {
+                        String qualifiedName = file.relativize(sourcePath).toString().replace("/", ".");
+                        qualifiedName = qualifiedName.substring(0, qualifiedName.length() - EXT_CLASS.length());
+                        discovered.add(new URLClassLocation(sourceURL, qualifiedName));
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return discovered;
     }
 
     @Override
