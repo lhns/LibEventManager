6953ab56f6881d8bbe1099c1a01dc75f6cf1ec78
LolHens <pierrekisters@gmail.com>
2014-07-21 14:40:59 +0200

    added classfile test moved test packages to their own source folder

---
+++ test/com/dafttech/classfile/test/IRunnableModule.java
@@ -0,0 +1,5 @@
+package com.dafttech.classfile.test;
+
+public interface IRunnableModule {
+    public void run();
+}
---
+++ test/com/dafttech/classfile/test/LoadedClass.java
@@ -0,0 +1,10 @@
+package com.dafttech.classfile.test;
+
+public class LoadedClass implements IRunnableModule {
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
+
+}
