83fffcb9001c849144477ef8ffe1892b26d18eab
LolHens <pierrekisters@gmail.com>
2015-11-27 15:52:48 +0100

    TrigonometryCache interpolation can now be disabled

--- src/main/java/org/lolhens/math/TrigonometryCache.java
+++ src/main/java/org/lolhens/math/TrigonometryCache.java
@@ -8,18 +8,34 @@ public class TrigonometryCache {
 
     private final float[] cache;
     private final float multiplier;
+    private final boolean interpolate;
 
-    public TrigonometryCache(int resolution) {
-        cache = new float[(int) Math.pow(2, resolution)];
+    public TrigonometryCache(int resolutionExp, boolean interpolate) {
+        this.interpolate = interpolate;
+        cache = new float[(int) Math.pow(2, resolutionExp)];
         multiplier = (float) ((1 / (Math.PI * 2)) * cache.length);
         for (int i = 0; i < cache.length; i++) cache[i] = (float) Math.sin(i / multiplier);
+    }
+
+    public TrigonometryCache(int resolutionExp) {
+        this(resolutionExp, true);
+    }
+
+    public TrigonometryCache(boolean interpolate) {
+        this(8, interpolate);
+    }
+
+    public TrigonometryCache() {
+        this(8, true);
     }
 
     public float sin(float val) {
         float index = val * multiplier;
         int floor = (int) index;
         int mod = floor & (cache.length - 1);
-        return cache[mod] + (index - floor) * (cache[mod + 1 == cache.length ? 0 : mod + 1] - cache[mod]);
+        if (interpolate)
+            return cache[mod] + (index - floor) * (cache[mod + 1 == cache.length ? 0 : mod + 1] - cache[mod]);
+        return cache[mod];
     }
 
     public float cos(float val) {
